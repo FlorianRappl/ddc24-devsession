233909b777acca63859177f8689481043d1f807c
Florian Rappl <mail@florian-rappl.de>
2024-11-28 01:05:43 +0000

    Added start of implementation

---
+++ project/Flixx/Catalog/Module.cs
@@ -0,0 +1,33 @@
+﻿namespace Catalog;
+
+public class Module : IMfModule
+{
+    public Module(IConfiguration configuration)
+    {
+        // Inject here what you want, e.g., the global `IConfiguration`.
+    }
+
+    public void Configure(IServiceCollection services)
+    {
+        // Configure your services in this function
+    }
+
+    public Task Setup(IMfAppService app)
+    {
+        app.AppendScript("swipe.js");
+
+        app.MapComponent<MoviesNavLink>("nav");
+        app.MapComponent<TvShowsNavLink>("nav");
+        app.MapRoute<Movies>();
+        app.MapRoute<TvShows>();
+
+        // Register components and more
+        return Task.CompletedTask;
+    }
+
+    public Task Teardown(IMfAppService app)
+    {
+        // Unregister things that need to be cleaned up
+        return Task.CompletedTask;
+    }
+}
---
+++ project/Flixx/Details/Module.cs
@@ -0,0 +1,26 @@
+﻿namespace Details;
+
+public class Module : IMfModule
+{
+    public Module(IConfiguration configuration)
+    {
+        // Inject here what you want, e.g., the global `IConfiguration`.
+    }
+
+    public void Configure(IServiceCollection services)
+    {
+        // Configure your services in this function
+    }
+
+    public Task Setup(IMfAppService app)
+    {
+        // Register components and more
+        return Task.CompletedTask;
+    }
+
+    public Task Teardown(IMfAppService app)
+    {
+        // Unregister things that need to be cleaned up
+        return Task.CompletedTask;
+    }
+}
